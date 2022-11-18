c4d4520742491d567cde574432046c3a04cab79b
HPCarey <hannahpcarey@gmail.com>
2022-11-18 13:29:59 +0000

    build tests to check for even number of evne numbers

--- evens.py
+++ evens.py
@@ -1,5 +1,4 @@
 def even_number_of_evens(numbers):
-    return None
     """
     Should raise a TypeError if a list is not passed into the
     error message: "A list was not passed into the funcion"
@@ -9,5 +8,12 @@ def even_number_of_evens(numbers):
     if the number of even numbers is even, return True
     """
 
+    if isinstance(numbers, list):
+        evens = (sum([1 for n in numbers if n % 2 == 0]))
+        
+        return True if evens and evens % 2 == 0 else False
+    else:
+        raise TypeError("A list was not passed into the function")    
+
 if __name__ == '__main__':
-    print(even_number_of_evens((5)))
+    even_number_of_evens([2, 1, 4])
